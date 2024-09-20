83a0d8cc4b50a82242d291dd5bd0fb59ee4e8823
Ivan Khudziakou <Ivan.Khudziakou@sap.com>
2024-09-20 11:37:37 -0400

    fix after the PR review

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, 'Please provide the name'],
+        required: [true, 'Please provide the email'],
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide the email'
         ],
